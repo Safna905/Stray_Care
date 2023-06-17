4d50ea8a97d4b334e9fe25ee03a5f685b673f314
Safna905 <eksafna786@gmail.com>
2023-06-17 12:55:09 +0530

    PHP

--- missing.php
+++ missing.php
@@ -3,19 +3,21 @@
 include 'connect.php';
 
 $User_id = $_POST['user_id'];
-$Animal_type = $_POST['animal_type'];
+//$Animal_type = $_POST['animal_type'];
 $Breed = $_POST['breed'];
 $Name = $_POST['name'];
-$Health_cond = $_POST['health_id'];
+//$Health_cond = $_POST['health_id'];
 $Color = $_POST['color'];
-$Lastseen_on = $_POST['lastseen_on'];
+$Lastseen_on = $_POST['missingDate'];
 $Report_date = $_POST['report_date'];
-$Image = $_POST['image'];
-$Status = $_POST['status'];
+//$Image = $_POST['image'];
+//$Status = $_POST['status'];
 $Mob_no = $_POST['mob_no'];
+$Lastseen_at = $_POST['location'];
 
 
-$sql = mysqli_query($con, "INSERT into missing_tb(user_id,animal_type,breed,name,health_cond,color,lastseen_on,report_date,image,status,mob_no)values ('$User_id','$Animal_type','$Breed','$Name','$$Health_cond','$Color','$Lastseen_on','$Report_date','$Image','$Status','$Mob_no');");
+
+$sql = mysqli_query($con, "INSERT into missing_tb(user_id,name,color,breed,lastseen_on,lastseen_at,mob_no,report_date) values ('$User_id','$Name','$Color','$Breed','$Lastseen_on','$Lastseen_at','$Mob_no','$Report_date');");
 
 if($sql) {
 
